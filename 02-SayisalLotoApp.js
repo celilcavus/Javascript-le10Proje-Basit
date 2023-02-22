f3de33f38e1332329dfc4bc58838ce120c082e0f
celil çavuş <celilcavus13@hotmail.com>
2023-02-22 16:45:36 +0300

    02-SayisalLotoApp Yorum satırı eklendi..

--- 02-SayisalLotoApp.js
+++ 02-SayisalLotoApp.js
@@ -1,13 +1,18 @@
-let myNumber =  229329;
-let gameNumber = 999999;
+let myNumber =  229329; // benim belirlemiş olduğum loto numarası
+let gameNumber = 999999; // lotonun maksimum alabiliceği değer
                 
-
+// sayısal değerleri random bir şekilde üretmesini sağlıyan fonksiyon(metot)
 function createRandomNumber(){
-   var no =  Math.floor(Math.random() * gameNumber) + 1;
+   var no =  Math.floor(Math.random() * gameNumber);
    return no;
 }
 
-
+/*
+NumberControl 90 adet rastgele loto numarası oluşturur.
+NumberControl fonksiyonu belirlemiş olduğunuz loto numranızı bu 90 adet rastgele
+numara ile karşılaştırır ve eğer doğru tahmin bulursanız döngüyü kırar ve ikramiyeyi kazanırsınız
+ben 10 defa denedim bana çıkmadı şans oyunu işte :)
+*/
 function NumberControl(number){
    let i = 1;
   
