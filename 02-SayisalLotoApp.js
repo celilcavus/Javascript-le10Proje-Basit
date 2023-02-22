114f8a94e5ebbbe1e604ed3fe4a9fb08041ef23a
celil çavuş <celilcavus13@hotmail.com>
2023-02-22 16:21:07 +0300

    sayisal loto uygulaması random özeligi test ediliyor

---
+++ 02-SayisalLotoApp.js
@@ -0,0 +1,14 @@
+let myNumber = 000000;
+let gameNumber = 000000;
+
+
+var numbers = function createRandomNumber(){
+   return Math.floor(Math.random() * 999999);
+}
+
+function NumberControl(youNumber){
+   let i = 0;
+   while (i>= 5) {
+      console.log(numbers);
+   }
+};
